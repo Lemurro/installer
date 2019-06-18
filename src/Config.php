bcd7dcab4f641725aea2b01ae0ea2ebe18e6aae8
Дмитрий Щербаков <atomcms@ya.ru>
2019-06-18 16:01:07 +0500

    fix Исправлена критичная ошибка с поиском каталога vendor

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 /**
  * Конфигурация
  *
- * @version 13.05.2019
+ * @version 18.06.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -13,7 +13,7 @@ namespace Lemurro\Installer;
  *
  * @package Lemurro\Installer
  *
- * @version 13.05.2019
+ * @version 18.06.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 class Config
@@ -21,5 +21,5 @@ class Config
     /**
      * @var string Версия инсталлера
      */
-    const VERSION = 'v1.0.0';
+    const VERSION = 'v1.0.1';
 }
